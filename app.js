5fee38b4dabcb0fa3a9d1705f94bba237ff1368d
Nopakorn <nopakorn.g@ssd.co.th>
2015-07-16 12:22:23 +0700

    init project
    initiated angular-material experiment project

---
+++ app.js
@@ -0,0 +1,34 @@
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(1337, '127.0.0.1');
+
+// console.log('Server running at http://127.0.0.1:1337/');
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var ext = /[\w\d_-]+\.[\w\d]+$/;
+
+http.createServer(function(req, res){
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream('index.html').pipe(res);
+    } else if (ext.test(req.url)) {
+        fs.exists(path.join(__dirname, req.url), function (exists) {
+            if (exists) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                fs.createReadStream('index.html').pipe(res);
+            } else {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                fs.createReadStream('404.html').pipe(res);
+            }
+        });
+    } else {
+        //  add a RESTful service
+    }
+}).listen(8000);
+
+
